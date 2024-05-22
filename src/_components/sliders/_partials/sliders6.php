e74d114ab0ef916a92a1deb4f3265f4f52b07410
raphael <raphmailhiot@gmail.com>
2024-05-21 20:57:51 -0400

    Save Sliders6

---
+++ src/_components/sliders/_partials/sliders6.php
@@ -0,0 +1,65 @@
+<section class="sliders6">
+  <div class="swiper" data-component="Carousel">
+    <div class="swiper-wrapper">
+
+      <div class="swiper-slide">
+
+        <img src="/assets/images/placeholder/sliders6-s1-bg.webp" alt="Cour arrière avec gazon traité par Club gazon">
+
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <img src="/assets/images/placeholder/sliders6-s1-img1.webp" alt="Couple qui à choisit Club Gazon">
+            </div>
+            <div class="col-md-6">
+              <p><span class="green-bg">Envie d'une pelouse verte cet été ?</span></p>
+              <p><span class="black-bg"></span>8 traitements</p>
+              <p><span class="red-bg">50% de rabais</span></p>
+
+              <a href="#" class="button">Découvrez les forfaits Club Gazon</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="swiper-slide">
+
+        <img src="/assets/images/placeholder/sliders6-s2-bg.webp" alt="Personne qui fait un traitement de gazon">
+
+        <div class="container">
+          <div class="w-40">
+            <p><span class="green-bg">Traitement contre mauvaises herbes</span></p>
+            <p><span class="red-bg">70% de rabais</span></p>
+            <a href="#" class="button">Découvrez les traitements</a>
+            <div class="row">
+              <div class="col-6">
+                <img src="/assets/images/placeholder/slider6-s2-img1.webp" alt="Gazon avant un traitement">
+                <p>Avant</p>
+              </div>
+              <div class="col-6">
+                <img src="/assets/images/placeholder/slider6-s2-img2.webp" alt="Gazon après un traitement">
+                <p>Après</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="swiper-slide">
+
+        <img src="/assets/images/placeholder/sliders6-s3-bg.webp" alt="Famille qui s'amuse sur leur pelouse">
+
+        <div class="text-center">
+          <p><span class="green-bg">Profitez d'un gazon vert cet été</span></p>
+          <p><span class="black-bg"></span>entretien de gazon</p>
+          <a href="#" class="button">Découvriez nos forfaits</a>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
+  <img src="/assets/images/placeholder/sliders6-gazon.webp" alt="Gazon">
+
+</section>
