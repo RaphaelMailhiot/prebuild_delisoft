7a3424ada56a8ab19f42e0044f4542d9c64b51d3
raphael <raphmailhiot@gmail.com>
2024-05-27 16:38:40 -0400

    Save Accueil 5

--- src/_components/accueil/accueil5.php
+++ src/_components/accueil/accueil5.php
@@ -1,1 +1,39 @@
-<h1>Accueil 5</h1>
+<!--EXEMPLE DE PAGE D'ACCUEIL 5-->
+<!--Assainiteck (https://assainiteck.ca/)-->
+
+<main class="accueil5">
+
+  <?php
+  $namebase = "Assainiteck";
+
+  /*--Slider--*/
+  include "_components/sliders/_partials/sliders3.php";
+
+  ?>
+
+  <section class="page-section-ptb accueil5-specialistes">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-5 order-md-switch accueil5-before-img">
+          <img class="img-fluid pr-30" src="/assets/images/placeholder/accueil/accueil5-specialistes-img.webp" alt="placeholder">
+        </div>
+        <div class="col-md-7">
+          <p class="seo-h2"><span class="theme-color">Spécialistes en désamiantage résidentiel</span> & décontamination
+            de moisissure</p>
+          <p>Les deux propriétaires, Danny et Frédéric, cumulent plus de 15 ans d’expérience dans l’industrie de la
+            construction et de l’après sinistre.</p>
+          <p>Passionnés par ces deux domaines connexes et tous deux ayant la fibre entrepreneuriale, lance l’entreprise
+            Assainiteck le 1er décembre 2021. Une entreprise spécialisée en enlèvement d’amiante et en décontamination
+            de moisissure.</p>
+          <a class="button button-border" href="#">En savoir plus</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php
+
+  /*Nos services*/
+  include "_components/services/_partials/services5.php";
+
+  ?>
