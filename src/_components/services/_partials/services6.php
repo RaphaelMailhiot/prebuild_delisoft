6013b68ca38587bc9165267d375d52bc9e9077e2
Raphaël <raph2003@live.ca>
2024-05-30 13:40:45 -0400

    Services6

---
+++ src/_components/services/_partials/services6.php
@@ -0,0 +1,29 @@
+<section class="page-section-ptb services-6">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-6 col align-self-center my-4">
+                <div class="agency-02-about-content-left">
+                    <h3> <i><img class="img-fluid" src="/assets/images/placeholder/services6-icone.webp" alt="Placeholder"> Titre</i></h3>
+                    <p class="mt-20 mb-30 xs-mb-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quidem reprehenderit suscipit? Accusamus aliquam, aliquid assumenda commodi eligendi error eveniet, libero quasi, reiciendis repudiandae tenetur?</p>
+                    <p class="mt-20 mb-30 xs-mb-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, magnam!</p>
+                </div>
+                <a class="button mb-0" href="#">Contactez-nous</a>
+            </div>
+            <div class="col-md-6 col-lg-6">
+                <img class="img-fluid h-100 w-100" src="/assets/images/placeholder/services6-img-1.webp" alt="Placeholder">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-md-6 order-md-switch">
+                <img class="img-fluid h-100 w-100" src="/assets/images/placeholder/services6-img-2.webp" alt="Placeholder">
+            </div>
+            <div class="col-lg-4 col-md-6 col align-self-center my-4">
+                <div class="agency-02-about-content-short">
+                    <h3 class="position-relative"> <i><img class="img-fluid" src="/assets/images/placeholder/services6-icone.webp" alt="Placeholder"> Titre</i></h3>
+                    <p class="mt-20 mb-30 xs-mb-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem fuga incidunt sunt tempore ut voluptates!</p>
+                    <p class="mt-20 mb-30 xs-mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dicta maxime sint sit sunt tempore unde voluptatibus! Aut id, magni?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
