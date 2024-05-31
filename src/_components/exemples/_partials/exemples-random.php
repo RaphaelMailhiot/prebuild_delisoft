21d525a0d00c44c55e5e4ae22f86afca0f496920
Raphaël <raph2003@live.ca>
2024-05-31 14:57:04 -0400

    Remove cache random NGINIX

--- src/_components/exemples/_partials/exemples-random.php
+++ src/_components/exemples/_partials/exemples-random.php
@@ -2,6 +2,10 @@
 
     <?php
 
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+
     // Récupérez tous les fichiers PHP dans le dossier
     $exemplesRandomSliders = glob('_components/sliders/_partials/*.php');
     $exemplesRandomFourSections = glob('_components/four-sections/_partials/*.php');
