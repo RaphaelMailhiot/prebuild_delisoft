59c29dde89cf538861bc4d0e81cfb9ee7250f8d9
raphael <raphmailhiot@gmail.com>
2024-06-04 18:23:44 -0400

    About1

--- src/_components/page/about/about1.php
+++ src/_components/page/about/about1.php
@@ -1,1 +1,152 @@
-<h1>About 1</h1>
+<main class="about1">
+
+  <!--Hero-->
+  <section class="page-title bg-overlay-black-40 parallax a-propos-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="page-title-name">
+            <h1 class="text-white">À propos</h1>
+            <p class="mb-0">Club Gazon</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--section couple-->
+  <section class="page-section-ptb xs-padding-base">
+    <div class="container xs-pt-40">
+      <div class="row a-propos-card">
+        <div class="col-lg-7 col-md-6 a-propos-card-img-left xs-pr-0 position-relative">
+          <img class="img-fluid img-couple" src="/assets/images/placeholder/sliders6-s1-img1.webp" alt="Placeholder">
+          <img class="img-fluid a-propos-img-left" src="https://clubgazon.com/images/a-propos/pourquoi-nous-choisir.webp"
+               alt="Placeholder">
+        </div>
+        <div class="col-lg-5 col-md-6 a-propos-card-texte">
+          <p class="seo-h2 mb-30 xs-mb-10">Pourquoi nous choisir</p>
+
+          <ul class="list list-unstyled">
+            <li><i class="fa fa-check-circle theme-color"></i> Un groupe d’experts pour mieux vous servir</li>
+            <li><i class="fa fa-check-circle theme-color"></i> Gamme de produit supérieur et respectueuse de
+              l’environnement
+            </li>
+            <li><i class="fa fa-check-circle theme-color"></i> Une équipe de techniciens certifiés</li>
+            <li><i class="fa fa-check-circle theme-color"></i> Services personnalisables selon vos besoins</li>
+            <li><i class="fa fa-check-circle theme-color"></i> Un service-conseil hors pairs via votre
+              espace-client
+            </li>
+            <li><i class="fa fa-check-circle theme-color"></i> Des programmes d’entretiens pour tous les budgets
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--Section theme-bg img-right-->
+  <section class="theme-bg page-section-ptb position-relative pb-100 pt-100 sm-pt-80 sm-pb-80 xs-pt-50 xs-pb-50">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 align-self-center pr-20 xs-mb-20">
+          <p class="seo-h2 mb-30 xs-mb-10 text-white">Un groupe d'experts pour mieux vous servir</p>
+          <p class="text-white">Nos experts dans le domaine de l'entretien de pelouse, possèdent des connaissances approfondies en
+            horticulture, des pratiques respectueuses de l'environnement, et une compréhension précise des
+            besoins spécifiques de chaque type de pelouse.</p>
+          <p class="text-white">Notre engagement envers la satisfaction du client, la personnalisation de nos services et
+            l'utilisation de techniques modernes pour assurer une pelouse saine et esthétique fait de nous un
+            chef de file dans le domaine.</p>
+        </div>
+
+        <div class="col-lg-6 col-md-6">
+          <img class="img-fluid border-radius" src="https://clubgazon.com/images/a-propos/groupe-expert.webp" alt="placeholder">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--section img-fancy-->
+  <section class="propos-fancy-container position-relative">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 a-propos-card-img-left xs-padding-base">
+          <img class="a-propos-img-fancy" src="https://clubgazon.com/images/a-propos/meilleurs-produits.webp" alt="placeholder">
+        </div>
+
+        <div class="col-lg-6 col-md-6 align-self-center pl-20 a-propos-fancy xs-mt-30">
+          <p class="seo-h2 mb-40 xs-mb-10">Les meilleurs produits sur le marché</p>
+          <p>Une combinaison de qualité, de performances et de résultats. Nos produits se distinguent par leur
+            efficacité et répondent aux divers besoins de chaque pelouse.</p>
+
+          <p>La réputation de la marque et les retours positifs de la part de nos clients contribuent à confirmer
+            que nos gammes de produits sont parmi les meilleurs disponibles.</p>
+        </div>
+
+      </div>
+
+      <div class="row pb-100 sm-mt-40 sm-pb-80 xs-pb-50">
+        <div class="col-lg-6 col-md-12 pr-60 sm-mb-20">
+          <p class="seo-h2 mb-30 xs-mb-10">Nos services sont personnalisés selon vos besoins</p>
+          <p>Notre entreprise s'engage à comprendre les besoins spécifiques de chaque client. Cela implique une
+            approche individualisée, où les services sont adaptés en fonction des besoins uniques de chaque
+            client.</p>
+          <p>La flexibilité de notre entreprise à répondre aux demandes particulières, l'écoute attentive
+            des besoins des clients, et la capacité à ajuster les services pour maximiser les résultats met en
+            évidence notre engagement à fournir des solutions adaptées et efficaces.</p>
+
+        </div>
+        <div class="col-lg-6 col-md-12">
+          <img class="img-fluid border-radius" src="https://clubgazon.com/images/a-propos/services-personnalises.webp" alt="placeholder">
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!--Section theme-bg img-left-->
+  <section class="theme-bg page-section-ptb position-relative pb-100 pt-100 sm-pt-80 sm-pb-80 xs-pt-50 xs-pb-50">
+    <div class="container">
+      <div class="row service-container">
+        <div class="col-lg-6 col-md-6">
+          <img class="img-fluid border-radius" src="https://clubgazon.com/images/a-propos/service-conseil-hors-pairs.webp" alt="Placeholder">
+        </div>
+
+        <div class="col-lg-6 col-md-6 align-self-center pl-60 xs-padding-base sm-pl-30 xs-mt-30">
+          <h2 class="mb-30 xs-mb-10 text-white">Un service conseil hors pairs</h2>
+          <p class="text-white">Nous possédons une compréhension approfondie des besoins particuliers de chaque pelouse, et offrons
+            des conseils adaptés à leur contexte.</p>
+
+          <p class="text-white">La disponibilité, la réactivité et la capacité à anticiper les besoins émergents font partie
+            intégrante de ce service. En outre, un engagement à surpasser les attentes contribuent à établir une
+            relation de confiance avec les clients.</p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!--Section img-right-->
+  <section class="page-section-ptb">
+    <div class="container">
+      <div class="row ">
+        <div class="col-lg-6 col-md-6 a-propos-card-texte xs-padding-base xs-pt-0 xs-pb-0">
+          <p class="seo-h2 mb-30 xs-mb-10">Des programmes d'entretien pour tous les budgets</p>
+          <p>Notre entreprise propose une gamme diversifiée de services adaptés à différentes capacités
+            financières.</p>
+          <p>Des programmes d'entretien de base à des services plus complets, permettant ainsi à une
+            clientèle aux budgets variés de trouver une solution qui correspond à ses moyens tout en maintenant
+            des normes de qualité élevées.</p>
+        </div>
+        <div class="col-lg-6 col-md-6 a-propos-card-img-right xs-padding-base xs-mt-10">
+          <img class="img-fluid border-radius" src="https://clubgazon.com/images/a-propos/programmes-entretien.webp" alt="Placeholder">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php
+  /*Action box*/
+  include "_components/section/actionbox/_partials/actionbox1.php"
+  ?>
+
+</main>
