47c164316522a30f10cd341e897892a9ab7a76ea
Raphaël <raph2003@live.ca>
2024-06-14 19:30:59 -0400

    Add about 2

--- src/_components/page/about/about2.php
+++ src/_components/page/about/about2.php
@@ -1,1 +1,209 @@
-<h1>About 2</h1>
+<?php
+
+$about2url = "https://z-chirotrottier.temoignagesvideo.com/";
+
+?>
+
+<!--hero-->
+<section class="page-title bg-overlay-black-20 parallax bg-propos">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-title-name">
+          <h1>À propos</h1>
+          <p>Chiropratique Trottier</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--Texte Image-->
+<section class="page-section-pt">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 align-self-center">
+        <div class="section-title mb-20">
+          <div class="seo-h6 subtitle theme-color">Chiropraticien Propriétaire</div>
+          <div class="seo-h2 title">Jonathan Trottier St-Pierre</div>
+        </div>
+        <p>C’est d’abord et avant tout par désir de vouloir aider les gens que je me suis dirigé dans le domaine de la
+          santé.</p>
+        <p>Après des études en science de l'activité physique avec une concentration en kinésiologie et une mineure en
+          massokinésiothérapie en 2014, j'ai réalisé que je devais approfondir mes connaissances pour mieux prendre soin
+          de mes patients. J'ai donc poursuivi un doctorat en chiropratique de 2014 à 2019, tout en participant à une
+          mission humanitaire en Bolivie en 2018.</p>
+        <p>Parallèlement, j'ai été membre de l'équipe de natation de l'UQTR de 2011 à 2014 et je continue à nager en
+          tant que loisir. En dehors des piscines, je suis sauveteur océanique et propriétaire de SN Hawaii, une
+          entreprise qui forme les jeunes sauveteurs québécois au brevet national option océanique sur les îles du
+          Pacifique.</p>
+        <img class="img-fluid" src="<?= $about2url ?>images/sign-trottier.png"
+             alt="Chiropraticien">
+      </div>
+      <div class="col-lg-6 align-self-end pt-80">
+        <img class="img-fluid" src="<?= $about2url ?>images/13.png" alt="Chiropraticien">
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--Quote-->
+<section class="page-section-ptb blockquote-section testimonial-title gray-bg">
+  <div class="container">
+    <div class="row no-gutter">
+      <div class="col-sm-12 text-center">
+        <blockquote class="blockquote quote mb-0">
+          Notre but est d’optimiser votre santé et de vous fournir les outils nécessaires pour atteindre votre bien-être
+          maximal. Au plaisir de vous rencontrer et de vous faire du bien !
+        </blockquote>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--Image Texte-->
+<section class="page-section-pt">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 align-self-end pt-80 order-lg-switch">
+        <img class="img-fluid" src="<?= $about2url ?>images/team/chiro-chanel.jpg"
+             alt="Chiropraticien">
+      </div>
+      <div class="col-lg-6 align-self-center">
+        <div class="section-title mb-20">
+          <div class="seo-h6 subtitle theme-color">Chiropraticienne</div>
+          <div class="seo-h2 title">Chanel Imbeau</div>
+        </div>
+        <p>Diplômée de l’UQTR en 2023, Dre Chanel Imbeau, chiropraticienne offre des soins professionnels et
+          personnalisés pour vous aider à retrouver votre bien-être.</p>
+        <p>Avec une passion pour la santé et le mouvement, notre praticienne s'engage à vous fournir des traitements de
+          haute qualité pour soulager vos douleurs et améliorer votre qualité de vie. Forte d'une formation rigoureuse
+          et d'une expertise acquise auprès des meilleurs professeurs et praticiens, notre chiropraticienne utilise une
+          approche holistique pour comprendre et traiter les causes sous-jacentes de vos maux.</p>
+        <br>
+        <p>Que vous souffriez de douleurs articulaires, de maux de dos, de tensions musculaires ou de tout autre
+          problème lié à la santé de votre colonne vertébrale, nous sommes là pour vous aider à retrouver votre mobilité
+          et votre confort. Chanel vous accompagnera dans votre parcours vers une meilleure santé en gardant comme
+          objectif de vous permettre de retrouver une vie active et sans douleur, en optimisant le fonctionnement de
+          votre corps et en favorisant votre bien-être global.</p>
+        <img class="img-fluid" src="<?= $about2url ?>images/sign.png" alt="Chiropraticien">
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--6 photos équipe-->
+<section class="page-section-ptb"
+         style="background-image:url(<?= $about2url ?>images/bg-dark-spine.jpg);background-repeat:no-repeat;background-size:cover;background-position: center left">
+  <div class="container">
+    <div class="row mb-30 equipe">
+      <div class="section-title text-center">
+        <div class="seo-h6 subtitle">Chiro Boucherville</div>
+        <div class="seo-h2 title text-white">Notre équipe</div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4 col-md-6 col-sm-6 mb-24">
+        <div class="team team-hover team-overlay rounded text-center m-0">
+          <div class="team-photo"><img class="img-fluid"
+                                       src="<?= $about2url ?>images/team/placeholder.jpg"
+                                       alt="Chiro Boucherville" style="box-sizing:border-box"></div>
+          <div class="team-description">
+            <div class="team-info">
+              <h5 class="text-white">Rebecca Torchon</h5>
+              <span class="text-white">Assistante</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 col-sm-6 mb-24">
+        <div class="team team-hover team-overlay rounded text-center m-0">
+          <div class="team-photo"><img class="img-fluid"
+                                       src="<?= $about2url ?>images/team/team1.jpg"
+                                       alt="Chiro Boucherville" style="box-sizing:border-box"></div>
+          <div class="team-description">
+            <div class="team-info">
+              <h5 class="text-white">Mireille Scotto</h5>
+              <span class="text-white">Assistante</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-6 mb-24">
+        <div class="team team-hover team-overlay rounded text-center m-0">
+          <div class="team-photo"><img class="img-fluid"
+                                       src="<?= $about2url ?>images/team/team3.jpg"
+                                       alt="Chiro Boucherville" style="box-sizing:border-box"></div>
+          <div class="team-description">
+            <div class="team-info">
+              <h5 class="text-white">Diane Tremblay</h5>
+              <span class="text-white">Massothérapeute</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-6 mb-24">
+        <div class="team team-hover team-overlay rounded text-center m-0">
+          <div class="team-photo"><img class="img-fluid"
+                                       src="<?= $about2url ?>images/team/placeholder.jpg"
+                                       alt="Chiro Boucherville" style="box-sizing:border-box"></div>
+          <div class="team-description">
+            <div class="team-info">
+              <h5 class="text-white">Nathalie Vallée</h5>
+              <span class="text-white">Massothérapeute</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-6 mb-24">
+        <div class="team team-hover team-overlay rounded text-center m-0">
+          <div class="team-photo">
+            <img class="img-fluid" src="<?= $about2url ?>images/team/team2.jpg"
+                 alt="Chiropraticienne" style="box-sizing:border-box">
+          </div>
+          <div class="team-description">
+            <div class="team-info">
+              <h5 class="text-white">Chanel Imbeau</h5>
+              <span class="text-white">Chiropraticienne</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 col-sm-6 mb-24">
+        <div class="team team-hover team-overlay rounded text-center m-0">
+          <div class="team-photo">
+            <img class="img-fluid"
+                 src="<?= $about2url ?>images/team/team5.jpg"
+                 alt="Chiro Boucherville" style="box-sizing:border-box"></div>
+          <div class="team-description">
+            <div class="team-info">
+              <h5 class="text-white">Jonathan Trottier St-Pierre</h5>
+              <span class="text-white">Chiropraticien propriétaire</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+<section class="action-box pattern full-width services-cta" style="background: url(<?= $about2url ?>images/bg/bg-spine-pen.png); background-repeat:no-repeat;background-size:cover;background-position:90%;">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 position-relative">
+        <div class="action-box-text text-white">
+          <h3 class="text-white">Centre chiropratique familial Trottier</h3>
+          <p class="text-white">Appelez dès maintenant afin de profiter d'un service exceptionnel et répondre à tous vos
+            besoins.</p>
+        </div>
+        <div class="action-box-button">
+          <a class="button bnt-menu-contact" href="tel:4506414455">Prendre un rendez-vous <i class="ti-mobile"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
