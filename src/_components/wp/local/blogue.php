6c282829b38f580c69bcba322cd3852db3e052ba
Raphaël <raph2003@live.ca>
2024-06-14 18:40:32 -0400

    Ajout pagination blogue

--- src/_components/wp/local/blogue.php
+++ src/_components/wp/local/blogue.php
@@ -3,11 +3,44 @@
 //TODO include le fichier wp-load.php
 require_once $wpUrl . "wp-load.php";
 
+//TODO mettre le nombre d'article par page
+$posts_per_page = 6;
+
+/*Pour get la page*/
+$blogPage = isset($_SERVER['REDIRECT_QUERY_STRING']) ? preg_replace('/\D/', '', $_SERVER['REDIRECT_QUERY_STRING']) : 1;
+
+// Fonction pour obtenir le nombre d'articles dans une catégorie par son nom
+function get_post_count_in_category_by_name($category_name) {
+  // Obtenir l'ID de la catégorie en utilisant son nom
+  $category_id = get_cat_ID($category_name);
+
+  // Vérifier si la catégorie existe
+  if (!$category_id) {
+    return 0;
+  }
+
+  // Obtenir les informations de la catégorie
+  $category = get_category($category_id);
+
+  // Retourner le nombre d'articles publiés dans la catégorie
+  return $category->count;
+}
+
+// Utilisation de la fonction pour obtenir le nombre d'articles dans la catégorie
+$post_count = get_post_count_in_category_by_name($category_name);
+
+// Pour le nombre de pages
+$nombrePage = ceil($post_count / $posts_per_page);
+
+$previousPage = $blogPage > 1 ? $blogPage - 1 : false;
+$nextPage = $blogPage < $nombrePage ? $blogPage + 1 : false;
+
 $args = array(
-  'posts_per_page' => -1, // -1 pour récupérer tous les articles
+  'posts_per_page' => $posts_per_page, // -1 pour récupérer tous les articles
   'post_status' => 'publish', // Seulement les articles publiés
   'post_type' => 'post', // Assurez-vous de cibler le type 'post' ou un autre type personnalisé si nécessaire
-  'category_name' => $lang // TODO mettre pour gérer les langues (mettre slug catégorie → {fr, en, etc.})
+  'category_name' => $lang, // TODO mettre pour gérer les langues (mettre slug catégorie → {fr, en, etc.})
+  'paged' => $blogPage,
 );
 
 $posts = get_posts($args);
@@ -37,13 +70,13 @@ $posts = get_posts($args);
         <?php
         if ($posts) {
           // Afficher les articles
-          foreach ($posts as $post) {
+          foreach ($posts as $k => $post) {
             setup_postdata($post);
 
             $thumbnail_id = get_post_thumbnail_id($post->ID);
             if ($thumbnail_id) {
               // Récupérer l'URL de l'image à la une
-              $image_url = wp_get_attachment_image_url($thumbnail_id, 'full');
+              $image_url = wp_get_attachment_image_url($thumbnail_id, 'medium_large');
               // Récupérer le texte alternatif de l'image
               $image_alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
             } else {
@@ -56,7 +89,7 @@ $posts = get_posts($args);
             <div class="col-sm-6">
               <div class="blog-entry mb-50">
                 <div class="entry-image clearfix">
-                  <img class="img-fluid" src="<?= $image_url ?>" alt="<?= $image_alt ?>">
+                  <img class="img-fluid" src="<?= $image_url ?>" alt="<?= $image_alt ?>" <?php if ($k > 4) { echo 'loading="lazy"'; } ?>>
                 </div>
                 <div class="blog-detail">
                   <div class="entry-title mb-10">
@@ -79,8 +112,28 @@ $posts = get_posts($args);
                 </div>
               </div>
             </div>
+          <?php } ?>
+          <div>
+            <nav aria-label="Pagination articles">
+              <ul class="pagination justify-content-center">
+                <li class="page-item <?php if (!$previousPage) { echo "disabled"; }?>">
+                  <a class="page-link" href="<?php echo $previousPage ? $url . $meta['blogue'][$lang] . '?blog_page=' . $previousPage : '#'; ?>" <?php if (!$previousPage) { echo 'tabindex="-1"'; } ?>>Précédente</a>
+                </li>
 
-          <?php }
+                <?php for ($i = 1; $i <= $nombrePage; $i++) : ?>
+                  <li class="page-item <?php if ($i == $blogPage) { echo "active"; }?>">
+                    <a class="page-link" href="<?= $url . $meta['blogue'][$lang] . '?blog_page=' . $i ?>"><?= $i ?></a>
+                  </li>
+                <?php endfor; ?>
+
+                <li class="page-item <?php if (!$nextPage) { echo "disabled"; }?>">
+                  <a class="page-link" href="<?php echo $nextPage ? $url . $meta['blogue'][$lang] . '?blog_page=' . $nextPage : '#'; ?>" <?php if (!$nextPage) { echo 'tabindex="-1"'; } ?>>Suivante</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        <?php } else {
+          echo "Aucun article trouvé ou erreur de requête.";
         }
         wp_reset_postdata(); ?>
 
