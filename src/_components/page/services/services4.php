7a09fe4536ed9e1c63bee6f2e3f746e360850734
raphael <raphmailhiot@gmail.com>
2024-06-17 19:38:59 -0400

    Ajout de services

--- src/_components/page/services/services4.php
+++ src/_components/page/services/services4.php
@@ -4,16 +4,16 @@ $services4url = "https://visionere4.com/";
 
 ?>
 
-<main class="page-services4">
+<main class="page4">
 
   <section class="page-section-ptb bg-overlay-black-50 jarallax page-services4-hero" data-speed="0.6"
-           style="background-image: url(<?= $services4url ?>images/formations/formations-titlebar-bg-min.jpg);;">
+           style="background-image: url(<?= $services4url ?>images/formations/formations-titlebar-bg-min.jpg);">
     <div class="container">
       <div class="row justify-content-center mb-30">
         <div class="col-lg-8 text-center">
           <div class="section-title mb-30 xs-mb-0">
             <h1 class="title text-white">Nos types de <span
-                class="theme-bg theme-color-dark pl-15 pr-15">formations</span></h1>
+                class="theme-bg theme-secondary-color pl-15 pr-15">formations</span></h1>
           </div>
         </div>
       </div>
@@ -49,5 +49,63 @@ $services4url = "https://visionere4.com/";
       </div>
     </div>
   </section>
+
+  <section class="page-section-ptb gray-bg">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-12 text-center">
+          <div class="section-title mb-40 xs-mb-20">
+            <h2 class="title theme-color mb-30">Formations <span class="theme-secondary-bg text-white pl-15 pr-15">en ligne</span>
+              <i>(E-learning)</i></h2>
+          </div>
+        </div>
+        <div class="col-lg-8 text-center">
+          <div class="js-video [youtube, widescreen]">
+            <iframe src="https://www.youtube.com/embed/MLS09y85lj0?si=8xrrNzBnRHhGq91y" title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen=""></iframe>
+          </div>
+
+          <div class="row justify-content-center mt-30">
+            <div class="col-lg-5 col-md-6 text-center">
+              <a class="button medium" href="#" target="_blank">Catalogue de formations</a>
+            </div>
+            <div class="col-lg-5 col-md-6 text-center">
+              <a class="button gray medium" href="#" target="_blank">Nos démonstrations</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="new-split-section right">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-md-5 col-sm-12 text-center shop-split-content">
+          <div class="new-split-section-texte">
+            <div class="section-title mb-30">
+              <div class="seo-h6 subtitle theme-color-dark">Contactez-nous</div>
+              <div class="seo-h1 title theme-color">Découvrez nos <span class="theme-secondary-bg text-white pl-15 pr-15">démos</span></div>
+              <p class="theme-color-dark">Remplissez le formulaire ci-dessous pour recevoir un <strong>aperçu d'une formation interactive en ligne</strong>.</p>
+            </div>
+
+            <!--TODO mettre un zoho form-->
+
+          </div>
+        </div>
+        <div class="col-md-6 col-sm-12 col-img">
+          <img class="img-fluid new-split-section-image" src="<?= $services4url ?>/images/formations/formations-demos-min.jpg"
+               alt="Placeholder">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php
+
+  include "_components/section/avantages/_partials/avantages1.php";
+
+  ?>
 
 </main>
--- src/_components/page/services/services5.php
+++ src/_components/page/services/services5.php
@@ -1,1 +1,56 @@
-<h1>Services 5</h1>
+<?php
+
+$services5url = "https://assainiteck.ca/";
+
+?>
+
+<main class="page5">
+
+  <section class="page-title pt-80 pb-30 parallax"
+           style="background: url(<?= $services5url ?>images/bg/placeholder-bg.jpg)">
+    <div class="container">
+      <div class="page-title-name">
+        <h1>Nos services</h1>
+        <p>Assainiteck</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="page-section-ptb accueil5-specialistes">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6 order-md-switch accueil5-before-img">
+          <img class="img-fluid img-cover" src="/assets/images/placeholder/accueil/accueil5-specialistes-img.webp"
+               alt="placeholder">
+        </div>
+        <div class="col-lg-6 col-md-6 align-self-center">
+          <div class="section-title">
+            <h6 class="theme-color">Assainiteck</h6>
+            <h2>Désamiantage</h2>
+          </div>
+          <p>La sécurité étant notre priorité, tous nos travaux de désamiantage seront effectués en stricte
+            conformité avec les normes de sécurité en vigueur.</p>
+          <p>Notre personnel est formé aux dernières pratiques de désamiantage, et nous utilisons des équipements
+            de pointe pour assurer la sécurité de tous les intervenants.</p>
+          <ul class="list list-mark">
+            <li>Inspection approfondie pour identifier la présence d'amiante</li>
+            <li>Mise en œuvre du plan de désamiantage par des professionnels qualifiés</li>
+            <li>Établissement d'un rapport détaillé sur les zones concernées</li>
+            <li>Transport des déchets vers des sites d'élimination autorisés</li>
+            <li>Émission de certificats de conformité</li>
+          </ul>
+          <a class="button" href="https://assainiteck.ca/contact">Contactez nous</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php
+
+  /*Avantages*/
+  include "_components/section/avantages/_partials/avantages3.php";
+
+  ?>
+
+
+</main>
